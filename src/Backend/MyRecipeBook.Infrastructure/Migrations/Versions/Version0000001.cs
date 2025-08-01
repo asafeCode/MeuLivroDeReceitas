9e6c5d9d5c72d8cf749a97b997cf38c1d58fe58b
asafeCode <matt.asafe@gmail.com>
2025-08-01 18:43:13 -0300

    feat: introduzindo fluent migrator para simplificar migracoes de tabela no database

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,11 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+public class Version0000001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        throw new NotImplementedException();
+    }
+}
