ba0ba6578d0343e2f2e28371b0792ab78f45355d
asafeCode <matt.asafe@gmail.com>
2025-08-01 16:20:03 -0300

    feat: feita a criaçao do schema no SqlServer com Dapper

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+
+//Criar Schemas e tabelas por migrations
+
+public static class DatabaseMigration
+{
+    public static void Migrate(string connectionString)
+    {
+        EnsureDatabaseCreated_SqlServer(connectionString);
+    }
+
+    private static void EnsureDatabaseCreated_SqlServer(string connectionString)
+    {
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+        var databaseName = connectionStringBuilder.InitialCatalog;
+
+        connectionStringBuilder.Remove("Initial Catalog");
+
+        using var dbConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+        var parameters = new DynamicParameters();
+        parameters.Add("name", databaseName);
+
+        var records = dbConnection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+
+        if (records.Any() == false)
+        {
+            dbConnection.Execute($"CREATE DATABASE {databaseName}");
+        }
+
+
+    }
+}
