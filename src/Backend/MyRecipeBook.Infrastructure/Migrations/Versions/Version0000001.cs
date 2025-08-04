7a8e0cb63690c0a9954770b0ca1fce6267976417
asafeCode <matt.asafe@gmail.com>
2025-08-04 17:46:43 -0300

    versao do migrator

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -2,10 +2,11 @@
 
 namespace MyRecipeBook.Infrastructure.Migrations.Versions;
 
+[Migration(DatabaseVersions.USER_TABLE, "Create a table to save the user's information")]
 public class Version0000001 : ForwardOnlyMigration
 {
     public override void Up()
     {
-        throw new NotImplementedException();
+        
     }
 }
