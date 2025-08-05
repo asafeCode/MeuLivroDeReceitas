3e01eb96d8d425cbdf7268da517731ddd95066ca
asafeCode <matt.asafe@gmail.com>
2025-08-05 18:26:56 -0300

    feat: introduzindo testes de unidade na Validacao de registro de usuario e Faker com Bogus

---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace Validators.Test.User.Register;
+
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Instanciar o que ele precisa
+        var validator = new RegisterUserValidator();
+
+        var request = RequestUserRegisterJsonBuilder.Build();
+        
+        var result = validator.Validate(request);
+        
+        // Assert
+        
+        Assert.True(result.IsValid);
+    }
+}
