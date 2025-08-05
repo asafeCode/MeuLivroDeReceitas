631dce5a9bb6b87882c886634065037c098c9ffe
asafeCode <matt.asafe@gmail.com>
2025-08-05 16:52:10 -0300

    feat: implementada a tabela Users

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -3,10 +3,13 @@
 namespace MyRecipeBook.Infrastructure.Migrations.Versions;
 
 [Migration(DatabaseVersions.USER_TABLE, "Create a table to save the user's information")]
-public class Version0000001 : ForwardOnlyMigration
+public class Version0000001 : VersionBase
 {
     public override void Up()
     {
-        
+        CreateTable("Users")
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
     }
 }
