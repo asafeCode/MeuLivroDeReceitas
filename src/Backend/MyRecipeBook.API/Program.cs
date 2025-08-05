e7019aab069897b6a37c600d4f9855443be09258
asafeCode <matt.asafe@gmail.com>
2025-08-05 17:20:11 -0300

    feat: Finalizado o Migrate

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -44,5 +44,8 @@ app.Run();
 void MigrateDatabase()
 {
     var connectionString = builder.Configuration.ConnectionString();
-    DatabaseMigration.Migrate(connectionString!);
+
+    var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    
+    DatabaseMigration.Migrate(connectionString!, serviceScope.ServiceProvider);
 }
