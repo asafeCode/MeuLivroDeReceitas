98262ec19cc8845b4f52f3b3c1b0ebd84a99ede1
asafeCode <matt.asafe@gmail.com>
2025-08-08 12:47:24 -0300

    feat: teste de unidade para o caso de uso de registro de usuario

---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,35 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using Shouldly;
+
+namespace UseCases.Test.User.Register;
+
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestUserRegisterJsonBuilder.Build();
+       
+        var useCase = CreateUseCase();
+        
+        var result = await useCase.Execute(request);
+        
+        result.ShouldNotBeNull();
+        result.Name.ShouldBeSameAs(request.Name);
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var readOnly = new UserReadOnlyRepositoryBuilder().Build();
+        var writeOnly = UserWriteOnlyRepositoryBuilder.Build();   
+        var mapper = MapperBuilder.Build();
+        
+        return new RegisterUserUseCase(readOnly, writeOnly, mapper, passwordEncripter, unitOfWork);
+    }
+}
