828f480a0cd08df1a3a1f436df31071df961b414
asafeCode <matt.asafe@gmail.com>
2025-08-28 17:35:12 -0300

    fix: appsettings bancoDeDados

--- src/Shared/MyRecipeBook.Communication/Requests/RequestUserRegisterJson.cs
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestUserRegisterJson.cs
@@ -1,7 +1,10 @@
-﻿namespace MyRecipeBook.Communication.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRecipeBook.Communication.Requests;
 
 public class RequestUserRegisterJson
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
