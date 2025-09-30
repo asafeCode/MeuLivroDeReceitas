4e7ad4e395bc88ee3ad54ffb425b3960d0595999
asafeCode <matt.asafe@gmail.com>
2025-09-30 18:00:39 -0300

    tests: error for empty title and invalid Tokens for webApi tests for Register Recipes

--- tests/WebApi.Test/Recipe/Register/RegisterRecipeTest.cs
+++ tests/WebApi.Test/Recipe/Register/RegisterRecipeTest.cs
@@ -1,8 +1,11 @@
+using System.Globalization;
 using System.Net;
 using System.Text.Json;
 using CommonTestUtilities.Requests;
 using CommonTestUtilities.Tokens;
+using MyRecipeBook.Exceptions;
 using Shouldly;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.Recipe.Register;
 
@@ -35,5 +38,28 @@ public class RegisterRecipeTest : MyRecipeBookClassFixture
         
         title.ShouldNotBeNullOrWhiteSpace(); title.ShouldBe(request.Title);
         id.ShouldNotBeNullOrWhiteSpace();
+    }    
+    
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string culture)
+    {
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Title = string.Empty;
+        
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userId);
+        
+        var response = await DoPost(method: Method, request: request, token: token, culture: culture);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        
+        var error = responseData.RootElement.GetProperty("errors").EnumerateArray();
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("RECIPE_TITLE_EMPTY", new CultureInfo(culture));
+        
+        error.ShouldHaveSingleItem(); error.ShouldContain(jsonElement => jsonElement.GetString()!.Equals(expectedMessage));
     }
 }
