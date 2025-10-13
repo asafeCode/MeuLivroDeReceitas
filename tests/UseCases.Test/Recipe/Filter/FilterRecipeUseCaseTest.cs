10d3e8f53e5559995ac401e989c33547fe8b2a9f
asafeCode <matt.asafe@gmail.com>
2025-10-13 16:37:25 -0300

    tests: finalizado teste de unidade para Filtro de Receitas

--- tests/UseCases.Test/Recipe/Filter/FilterRecipeUseCaseTest.cs
+++ tests/UseCases.Test/Recipe/Filter/FilterRecipeUseCaseTest.cs
@@ -4,13 +4,12 @@ using CommonTestUtilities.MapConfiguration;
 using CommonTestUtilities.Repositories.Recipe;
 using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.Recipe.Filter;
+using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Exceptions;
 using MyRecipeBook.Exceptions.ExceptionsBase;
-using MyRecipeBook.Infrastructure.DataAccess.Repositories;
 using Shouldly;
 
 namespace UseCases.Test.Recipe.Filter;
-
 public class FilterRecipeUseCaseTest : MapperForUseCaseTests
 {
     [Fact]
@@ -18,35 +17,39 @@ public class FilterRecipeUseCaseTest : MapperForUseCaseTests
     {
         var (user, _) = UserBuilder.Build();
         var request = RequestFilterRecipeJsonBuilder.Build();
-        var useCase = CreateUseCase(user);
-        var response = await useCase.Execute(request);
-        
-        response.ShouldNotBeNull();
+        var recipes = RecipeBuilder.Collection(user);
+        var useCase = CreateUseCase(user, recipes);
 
-    }    
-    
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Recipes.ShouldNotBeNull();
+        result.Recipes.Count.ShouldBe(recipes.Count);
+    }
+
     [Fact]
-    public async Task Error_Title_Empty()
+    public async Task Error_CookingTime_Invalid()
     {
         var (user, _) = UserBuilder.Build();
-        
+        var recipes = RecipeBuilder.Collection(user);
         var request = RequestFilterRecipeJsonBuilder.Build();
-        
-        var useCase = CreateUseCase(user);
-        Func<Task> act = async () => await useCase.Execute(request);
+        request.CookingTime.Add((ComCookingTime)1000);
+        var useCase = CreateUseCase(user, recipes);
 
+        Func<Task> act = async () => await useCase.Execute(request);
         var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
         
         exception.GetErrorMessage().Count.ShouldBe(1);
-        exception.GetErrorMessage().ShouldContain(ResourceMessagesException.RECIPE_TITLE_EMPTY);
+        exception.GetErrorMessage().ShouldContain(ResourceMessagesException.COOKING_TIME_NOT_SUPPORTED);
     }
-    
-    private static FilterRecipeUseCase CreateUseCase(MyRecipeBook.Domain.Entities.User user,
+
+    private static FilterRecipeUseCase CreateUseCase(
+        MyRecipeBook.Domain.Entities.User user,
         IList<MyRecipeBook.Domain.Entities.Recipe> recipes)
     {
         var loggedUser = LoggedUserBuilder.Build(user);
         var repository = new RecipeReadOnlyRepositoryBuilder().Filter(user, recipes).Build();
-        
+
         return new FilterRecipeUseCase(loggedUser, repository);
     }
 }
